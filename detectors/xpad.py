5ed415ffbd2df71534d0d0022298ede8630a4a31
Alexandre Marie <alexandre.marie@synchrotron-soleil.fr>
2020-09-25 11:36:02 +0200

    Added xpad and cirpad python files to visualise images according to the chosen detector

---
+++ detectors/xpad.py
@@ -0,0 +1,34 @@
+from PyQt5.QtWidgets import QPushButton, QWidget, QTabWidget, QVBoxLayout
+from PyQt5.QtCore import pyqtSlot
+
+class XpadContext(QWidget):
+
+    def __init__(self):
+        super(QWidget, self).__init__()
+        self.layout = QVBoxLayout(self)
+
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.tabs.resize(400,300)
+
+        # Add tabs
+        self.tabs.addTab(self.tab1,"Initial Data")
+        self.tabs.addTab(self.tab2,"Visualisation and processing")
+
+        # Create first tab
+        self.tab1.layout = QVBoxLayout(self)
+        self.pushButton1 = QPushButton("PyQt5 button")
+        self.tab1.layout.addWidget(self.pushButton1)
+        self.tab1.setLayout(self.tab1.layout)
+
+        # Add tabs to widget
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
+
+    @pyqtSlot()
+    def on_click(self):
+        print("\n")
+        for currentQTableWidgetItem in self.tableWidget.selectedItems():
+            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
