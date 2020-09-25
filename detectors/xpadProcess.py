2cecfced5b1a34f99225e36e44c3bfafa24806d6
Alexandre Marie <alexandre.marie@synchrotron-soleil.fr>
2020-09-25 11:50:55 +0200

    Removed redundant layout settings and added new qtabwidget to the visualisation tab

--- detectors/xpadProcess.py
+++ detectors/xpadProcess.py
@@ -1,9 +1,7 @@
 from PyQt5.QtWidgets import QPushButton, QWidget, QTabWidget, QVBoxLayout
 from PyQt5.QtCore import pyqtSlot
 
-from detectors.xpadProcess import XpadVisualisation
-
-class XpadContext(QWidget):
+class XpadVisualisation(QWidget):
 
     def __init__(self):
         super(QWidget, self).__init__()
@@ -13,21 +11,18 @@ class XpadContext(QWidget):
         self.tabs = QTabWidget()
         self.tab1 = QWidget()
         self.tab2 = QWidget()
+        self.tab3 = QWidget()
         self.tabs.resize(400,300)
 
         # Add tabs
-        self.tabs.addTab(self.tab1,"Initial Data")
-        self.tabs.addTab(self.tab2,"Visualisation and processing")
+        self.tabs.addTab(self.tab1,"Raw data")
+        self.tabs.addTab(self.tab2,"Unfolded data")
+        self.tabs.addTab(self.tab3,"Fitted data")
 
         # Create first tab
         self.tab1.layout = QVBoxLayout(self.tab1)
         self.pushButton1 = QPushButton("PyQt5 button")
         self.tab1.layout.addWidget(self.pushButton1)
-
-        # Create second tab
-        self.tab2.layout = QVBoxLayout(self.tab2)
-        self.widget = XpadVisualisation()
-        self.tab2.layout.addWidget(self.widget)
 
         # Add tabs to widget
         self.layout.addWidget(self.tabs)
