85d2149a711c25cf2bc1d14a934354b03cb98b17
Alexandre Marie <alexandre.marie@synchrotron-soleil.fr>
2020-09-25 15:46:54 +0200

    Added scan types into the constants file

--- constants.py
+++ constants.py
@@ -4,6 +4,11 @@ class detectors(Enum):
       XPAD = "XPAD"
       CIRPAD = "CIRPAD"
 
+class scan_types(Enum):
+      ASCAN = "ascan"
+      TSCAN = "tscan"
+      FSCAN = "fscan"
+
 class fitting_curves(Enum):
       GAUSSIAN = "GAUSSIAN"
       GAUSSIANb = "GAUSSIANb"
