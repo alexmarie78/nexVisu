7d64664d97dbca3d77a91c1465da308969d4721c
AMA <alexmarie78@gmail.com>
2020-12-15 18:20:40 +0100

    WIP Diffraction diagram 0-40°

--- src/detectors/xpad/xpadProcess.py
+++ src/detectors/xpad/xpadProcess.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QWidget, QTabWidget, QVBoxLayout, QInputDialog, QLin
 from PyQt5.QtCore import pyqtSlot, QTimer
 from silx.gui.plot import Plot1D
 from pyFAI.azimuthalIntegrator import AzimuthalIntegrator
+from pyFAI.detectors._imxpad import ImXPadS140
 
 from src.constants import DataPath
 from src.utils.dataViewers import RawDataViewer, UnfoldedDataViewer
@@ -18,7 +19,7 @@ class XpadVisualisation(QWidget):
     def __init__(self):
         super(QWidget, self).__init__()
         self.layout = QVBoxLayout(self)
-        self.azimuthalIntegrator = AzimuthalIntegrator()
+        self.azimuthalIntegrator = AzimuthalIntegrator(dist=0.1, detector=ImXPadS140())
         self.raw_data = None
         self.flatfield_image = None
         self.path = None
@@ -82,6 +83,7 @@ class XpadVisualisation(QWidget):
         self.path = path
         with File(os.path.join(path), mode='r') as h5file:
             self.raw_data = get_dataset(h5file, DataPath.IMAGE_INTERPRETATION.value)[:]
+            self.azimuthalIntegrator.wavelength = get_dataset(h5file, DataPath.WAVELENGTH.value)[0]
         # We put the raw data in the dataviewer
         self.raw_data_viewer.set_movie(self.raw_data)
         # We allocate a number of view in the stack
@@ -106,8 +108,6 @@ class XpadVisualisation(QWidget):
             # Collect the angles
             self.delta_array, self.gamma_array = get_angles(self.path)
 
-            print(self.delta_array)
-            print(self.gamma_array)
             # Populate the iterators that will help running the unfolding of data
             self.data_iterator = iter([image for image in self.raw_data])
             self.index_iterator = iter([i for i in range(self.raw_data.shape[0])])
@@ -124,7 +124,7 @@ class XpadVisualisation(QWidget):
             gamma = self.gamma_array[index] if len(self.gamma_array) > 1 else self.gamma_array[0]
             # Correct and unfold raw data
             unfolded_data = correct_and_unfold_data(self.geometry, image, delta, gamma)
-            self.diagram.append(numpy.cumsum(unfolded_data[2], axis=0))
+            self.diagram.append(self.azimuthalIntegrator.integrate1d(image, 2000    , unit="2th_deg", radial_range=(0, 40)))
             self.angles.append(unfolded_data[0][::self.scatter_factor])
 
             # Add the unfolded image to the scatter stack of image.
@@ -134,9 +134,23 @@ class XpadVisualisation(QWidget):
         except StopIteration:
             self.unfold_timer.stop()
             self.is_unfolding = False
-            #self.diagram_data_plot.addCurve(numpy.concatenate(self.angles, axis=None),
-            #                                numpy.concatenate(self.diagram, axis=None))
-            self.diagram_data_plot.addCurve(self.azimuthalIntegrator.integrate1d(numpy.concatenate(self.diagram, axis=None), 15000, radial_range=(0, max(self.angles))))
+            #data = numpy.asarray(self.diagram)
+            #print(type(data), data.shape, data.dtype)
+            # radial_range = (0, int(max(numpy.concatenate(self.angles, axis=None))))
+            # result = self.azimuthalIntegrator.integrate1d(data[0], 15000,
+            #                                             unit="2th_deg",
+            #                                             method="splitpixel",
+            #                                            radial_range=radial_range)
+            result_x = numpy.zeros((len(self.diagram[0][0]),))
+            result_y = numpy.zeros((len(self.diagram[0][0]),))
+            index = 0
+            for index, data in enumerate(self.diagram):
+                result_x += data[0]
+                result_y += data[1]
+                index = index
+            result_x = result_x/index
+            result_y = result_y/index
+            self.diagram_data_plot.addCurve(result_x, result_y)
 
     def get_flatfield(self, flat_img: numpy.ndarray):
         self.flatfield_image = flat_img
